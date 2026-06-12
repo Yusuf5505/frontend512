6919b4d7769fed6313a8822aa35ca57058146575
Yusuf <usufsajganov85@gmail.com>
2026-06-12 15:39:21 +0300

    Задание

---
+++ home__work__30__js/script.js
@@ -0,0 +1,20 @@
+let email = document.querySelector("#email");
+email.addEventListener("input", function () {
+    let reg = /^([A-Za-z0-9#&_\-\.])+@([A-Za-z0-9#&_\-])+\.([A-Za-z]){2,}$/ig
+    let span=document.querySelector("span")
+    if (reg.test(email.value)) {
+        email.style.border = '1px solid green';
+        span.style.color="green"
+          span.textContent="Пароль под требования"
+        return true;
+      
+    } else {
+        span.style.color="red"
+          span.textContent="Слабый пароль"
+        email.style.border = '1px solid red';
+
+        return false;
+
+    }
+}
+)
